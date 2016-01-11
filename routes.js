b4ee0e9a8b9fa4c71497050327c2108d56d9048a
Winchester <nwinches@amazon.com>
2016-01-11 14:33:52 -0800

    destination update

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ module.exports = function(app){
   });
 
   app.get('/destinations/:destination_id', function (request, response) {
-    queries.getDestination(request.params.destination_id, function(err, table) {
+    queries.getDestination(request.params.destination_id, function(err, destination) {
       if (err) {
         console.error(err);
         response.send("Error " + err);
