283448a7db12d294bf5c70282a3986231145e490
Winchester <nwinches@amazon.com>
2016-01-10 19:42:44 -0800

    query update

--- queries.js
+++ queries.js
@@ -4,11 +4,32 @@ module.exports = {
     var pg = require('pg');
     var dbUrl = process.env.DATABASE_URL;
     console.error("connecting to: " + dbUrl);
-    
+
+    var queryString = 'select destination_id, destination_name, country_code, min_days, max_days, activity_name, description \
+                         from destinations \
+                              left outer join destination_activity using (destination_id) \
+                              left outer join activities using (activity_id)';
+
     pg.connect(dbUrl, function(err, client, done) {
-      client.query('SELECT * FROM test_table', function(err, result) {
+      client.query(queryString, function(err, results) {
         done();
-        callback(err, result);
+        
+        var table;
+        for (result in results) {
+          table[result.destination_id].destination_name = result.destination_name;
+          table[result.destination_id].country_code = result.country_code;
+          table[result.destination_id].min_days = result.min_days;
+          table[result.destination_id].max_days = result.max_days;
+          
+          if (!table[result.destination_id].activity_name) {
+            table[result.destination_id].activity_name = new Array;
+            table[result.destination_id].description = new Array;
+          }
+          table[result.destination_id].activity_name[table[result.destination_id].activity_name.length] = result.activity_name;
+          table[result.destination_id].description[table[result.destination_id].description.length] = result.description;
+        }
+
+        callback(err, table);
       });
     });
   }
