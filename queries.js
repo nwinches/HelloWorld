0404983813948034e586127eeaaef2cd7e1c71cf
Winchester <nwinches@amazon.com>
2016-01-11 12:16:00 -0800

    add destination_id to results

--- queries.js
+++ queries.js
@@ -24,6 +24,7 @@ module.exports = {
             entry = table[result['destination_id']];
           } else {
             table[result['destination_id']] = entry;
+            entry.destination_id = result['destination_id'];
             entry.destination_name = result['destination_name'];
             entry.country_name = result['country_name'];
             entry.min_days = result['min_days'];
@@ -69,6 +70,7 @@ module.exports = {
             entry = table[result['destination_id']];
           } else {
             table[result['destination_id']] = entry;
+            entry.destination_id = result['destination_id'];
             entry.destination_name = result['destination_name'];
             entry.country_name = result['country_name'];
             entry.min_days = result['min_days'];
